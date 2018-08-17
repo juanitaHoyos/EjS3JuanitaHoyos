b55a5ee1f9ce15b9a609093c13bef9bfd40b7960
juanitaHoyos <juanitahoyos1@gmail.com>
2018-08-17 15:59:43 -0500

    lo que hice en clase

--- app/src/main/java/com/example/estudiante/ejs3juanitahoyos/MainActivity.java
+++ app/src/main/java/com/example/estudiante/ejs3juanitahoyos/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.estudiante.ejs3juanitahoyos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_titulo;
+    EditText et_nombre;
+    Button btn_calcular;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_titulo = findViewById(R.id.tv_titulo);
+        et_nombre = findViewById(R.id.et_nombre);
+        btn_calcular = findViewById(R.id.btn_calcular);
+
+        btn_calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre= et_nombre.getText().toString();
+
+                tv_titulo.setText("hola "+ nombre);
+
+                Toast.makeText( MainActivity.this,  "hola "+nombre, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
